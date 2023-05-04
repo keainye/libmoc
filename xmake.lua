eec63293932d1a28ac9b78e01f69a4ec7a8cc2bd
Pokyux <shiftregister233@outlook.com>
2023-05-05 00:52:48 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,9 @@
 add_rules("mode.debug", "mode.release")
 
+add_headerfiles("src/mocutils")
+
 target("mocutils")
     set_kind("static")
-    add_includedirs({"src"})
     add_files("src/mocutils.cc")
 
 target("moctest")

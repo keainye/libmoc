118c738b2f9b271d3c90d004a47604a964d9f68f
Pokyux <pokyux@mail.ustc.edu.cn>
2024-05-27 16:03:07 +0800

    use ptr in semaphore mutex

--- src/semaphore.h
+++ src/semaphore.h
@@ -10,50 +10,56 @@ namespace moc {
 template <int _cap>
 struct semaphore{
   int cap, val;
-  std::queue<std::mutex> wait;
+  std::queue<std::mutex*> wait;
   std::mutex vlock, wlock;
 
   semaphore(): cap(_cap), val(0) {}
   semaphore(int _val): cap(_cap), val(_val) {}
 
   void acquire() {
-    std::mutex local;
+    std::mutex *local = nullptr;
     vlock.lock();
     wlock.lock();
     if (val < 0)
       moc::panic("internal bug: semaphore val < 0");
     if (val) {
       if (wait.size()) {
-        wait.front().unlock();
+        wait.front()->unlock();
         wait.pop();
       } else val -= 1;
     } else {
-      local.lock();
+      local = new std::mutex;
+      local->lock();
       wait.push(local);
     }
     wlock.unlock();
     vlock.unlock();
-    local.lock();
+    if (local == nullptr) return;
+    local->lock();
+    delete local;
   }
 
   void release() {
-    std::mutex local;
+    std::mutex *local = nullptr;
     vlock.lock();
     wlock.lock();
     if (val > cap)
       moc::panic("internal bug: semaphore val > cap");
     if (val < cap) {
       if (wait.size()) {
-        wait.front().unlock();
+        wait.front()->unlock();
         wait.pop();
       } else val += 1;
     } else {
-      local.lock();
+      local = new std::mutex;
+      local->lock();
       wait.push(local);
     }
     wlock.unlock();
     vlock.unlock();
-    local.lock();
+    if (local == nullptr) return;
+    local->lock();
+    delete local;
   }
 };
 

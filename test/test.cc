cbe555e64ceb8140f780c64b7c64f32eed9f45b6
Pokyux <pokyux@mail.ustc.edu.cn>
2024-05-27 18:06:50 +0800

    clean test.cc

--- test/test.cc
+++ test/test.cc
@@ -1,13 +1,5 @@
 #include "test.h"
-#include <semaphore.h>
 
 int main() {
-  moc::semaphore<2> s;
-  s.release();
-  printf("1");
-  s.release();
-  printf("1");
-  s.release();
-  printf("1");
   return 0;
 }

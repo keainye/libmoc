0f872c189d53546e30ea76268c5f6459f2f1d95b
Pokyux <pokyux@mail.ustc.edu.cn>
2024-05-27 16:07:55 +0800

    rename < >

--- src/channel.h
+++ src/channel.h
@@ -16,23 +16,24 @@ class channel {
     in.lock();
   }
 
-  void operator<(T _value);
-  void operator>(T &_value);
+  void operator<<(T _value);
+  void operator>>(T &_value);
 };
 
 template <typename T>
-void channel<T>::operator<(T _value) {
+void channel<T>::operator<<(T _value) {
   this->in.lock();
   this->content = _value;
   this->out.unlock();
 }
 
 template <typename T>
-void channel<T>::operator>(T &_value) {
+void channel<T>::operator>>(T &_value) {
   this->out.lock();
   _value = this->content;
   this->in.unlock();
 }
+
 };  // namespace moc
 
 #endif

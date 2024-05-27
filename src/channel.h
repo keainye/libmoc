449573dded0f5d0a62304ee7a32da33443d1d624
Pokyux <pokyux@mail.ustc.edu.cn>
2024-05-27 16:53:00 +0800

    feat: multi channel

--- src/channel.h
+++ src/channel.h
@@ -1,37 +1,108 @@
 #ifndef __MOC_CHANNEL_H
 #define __MOC_CHANNEL_H
 
-#include "mutex"
+#include <mutex>
+#include <utility>
+#include <queue>
+#include "semaphore.h"
 
 namespace moc {
+
 template <typename T>
 class channel {
-  T content;
-  std::mutex in, out;
-
+  std::queue<std::pair<std::mutex*, T>*> read, write;
+  std::mutex clock;
  public:
-  channel() { out.lock(); }
-  channel(T _value) {
-    content = _value;
-    in.lock();
-  }
-
   void operator<<(T _value);
   void operator>>(T &_value);
 };
 
+// write
 template <typename T>
 void channel<T>::operator<<(T _value) {
-  this->in.lock();
-  this->content = _value;
-  this->out.unlock();
+  std::pair<std::mutex*, T> *local = nullptr;
+  clock.lock();
+  if (read.size()) {
+    read.front()->second = _value;
+    read.front()->first->unlock();
+    read.pop();
+  } else {
+    local = new std::pair{new std::mutex, _value};
+    local->first->lock();
+    write.push(local);
+  }
+  clock.unlock();
+  if (local == nullptr) return;
+  local->first->lock();
+  delete local->first;
+  delete local;
 }
 
+// read
 template <typename T>
 void channel<T>::operator>>(T &_value) {
-  this->out.lock();
-  _value = this->content;
-  this->in.unlock();
+  std::pair<std::mutex*, T> *local = nullptr;
+  clock.lock();
+  if (write.size()) {
+    _value = write.front()->second;
+    write.front()->first->unlock();
+    write.pop();
+  } else {
+    local = new std::pair{new std::mutex, _value};
+    local->first->lock();
+    read.push(local);
+  }
+  clock.unlock();
+  if (local == nullptr) return;
+  local->first->lock();
+  _value = local->second;
+  delete local->first;
+  delete local;
+}
+
+template<typename T, int _cap>
+class channel {
+  T *content;
+  int cptr;
+  std::mutex clock;
+  semaphore<_cap> full, empty(_cap);
+public:
+  int cap, size;
+  channel(): cap(_cap), size(0), cptr(0);
+  ~channel();
+  void operator<<(T _value);
+  void operator>>(T &_value);
+};
+
+template<typename T, int _cap>
+channel<T, _cap>::channel(): cap(_cap), size(0), cptr(0) {
+  content = new T[_cap];
+}
+
+template<typename T, int _cap>
+channel<T, _cap>::~channel() {
+  delete[] content;
+}
+
+
+// write
+template<typename T, int _cap>
+void channel<T, _cap>::operator<<(T _value) {
+  empty.acquire();
+  clock.lock();
+  content[cptr++] = _value;
+  clock.unlock();
+  full.release();
+}
+
+// read
+template<typename T, int _cap>
+void channel<T, _cap>::operator>>(T &_value) {
+  full.acquire();
+  clock.lock();
+  _value = content[--cptr];
+  clock.unlock();
+  empty.release();
 }
 
 };  // namespace moc
--- test/channel.cc
+++ test/channel.cc
@@ -5,12 +5,17 @@ using namespace std;
 using namespace moc;
 
 int main() {
-  channel<int> chan;
-  chan < 233;
-  int i = 123;
-  chan > i;
-  test(i == 233);
-  pass("channel");
-
+  moc::channel<int, 3> c;
+  c << 1;
+  c << 2;
+  c << 3;
+  int recv;
+  c >> recv;
+  test(recv == 1);
+  c >> recv;
+  test(recv == 2);
+  c >> recv;
+  test(recv == 3);
+  pass("channel<typename, int>");
   return 0;
 }

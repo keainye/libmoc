6cdff5aa56bd7a7eb35afd9897eedbc70940df79
Pokyux <pokyux@mail.ustc.edu.cn>
2024-05-28 17:03:34 +0800

    add log format

--- src/log.cc
+++ src/log.cc
@@ -1,4 +1,5 @@
 #include "log.h"
+#include <stdarg.h>
 
 namespace moc {
 void log(std::string msg) {
@@ -11,6 +12,34 @@ void warn(std::string msg) {
 
 void panic(std::string msg) {
   std::printf("\033[31m[E] %s\n\033[0m", msg.c_str());
+  exit(1);
+}
+
+void logf(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  std::printf("\033[32m[I] ");
+  std::vprintf(fmt, args);
+  std::printf("\033[0m\n");
+  va_end(args);
+}
+
+void warnf(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  std::printf("\033[33m[W] ");
+  std::vprintf(fmt, args);
+  std::printf("\033[0m\n");
+  va_end(args);
+}
+
+void panicf(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  std::printf("\033[31m[E] ");
+  std::vprintf(fmt, args);
+  std::printf("\033[0m\n");
+  va_end(args);
   exit(1);
 }
 }

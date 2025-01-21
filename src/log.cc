fcbd9e5127395ba06a3c23e4900fc0c6429ffba7
Pokyux <pokyux@mail.ustc.edu.cn>
2025-01-21 11:47:45 +0800

    enhance log

--- src/log.cc
+++ src/log.cc
@@ -1,45 +1,72 @@
 #include "log.h"
 #include <stdarg.h>
+#include <ctime>
+#include <chrono>
 
 namespace moc {
+void _print_time() {
+	auto now = std::chrono::system_clock::now();
+	auto timestamp = std::chrono::system_clock::to_time_t(now);
+	std::tm local_time = *std::localtime(&timestamp);
+
+	auto duration = now.time_since_epoch();
+  auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration) % 1000;
+	std::printf(
+		"\033[34m%04d-%02d-%02d %02d:%02d:%02d.%03d\033[0m",
+		local_time.tm_year + 1900,
+		local_time.tm_mon + 1,
+		local_time.tm_mday,
+		local_time.tm_hour,
+		local_time.tm_min,
+		local_time.tm_sec,
+		millis.count()
+	);
+}
+
 void log(std::string msg) {
-  std::printf("\033[32m[I] %s\n\033[0m", msg.c_str());
+	_print_time();
+	std::printf("\033[32m [I] \n\033[0m%s", msg.c_str());
 }
 
 void warn(std::string msg) {
-  std::printf("\033[33m[W] %s\n\033[0m", msg.c_str());
+	_print_time();
+	std::printf("\033[33m [W] \n\033[0m%s", msg.c_str());
 }
 
 void panic(std::string msg) {
-  std::printf("\033[31m[E] %s\n\033[0m", msg.c_str());
-  exit(1);
+	_print_time();
+	std::printf("\033[31m [E] \n\033[0m%s", msg.c_str());
+	exit(1);
 }
 
 void logf(const char *fmt, ...) {
-  va_list args;
-  va_start(args, fmt);
-  std::printf("\033[32m[I] ");
-  std::vprintf(fmt, args);
-  std::printf("\033[0m");
-  va_end(args);
+	_print_time();
+	va_list args;
+	va_start(args, fmt);
+	std::printf("\033[32m [I] ");
+	std::printf("\033[0m");
+	std::vprintf(fmt, args);
+	va_end(args);
 }
 
 void warnf(const char *fmt, ...) {
-  va_list args;
-  va_start(args, fmt);
-  std::printf("\033[33m[W] ");
-  std::vprintf(fmt, args);
-  std::printf("\033[0m");
-  va_end(args);
+	_print_time();
+	va_list args;
+	va_start(args, fmt);
+	std::printf("\033[33m [W] ");
+	std::printf("\033[0m");
+	std::vprintf(fmt, args);
+	va_end(args);
 }
 
 void panicf(const char *fmt, ...) {
-  va_list args;
-  va_start(args, fmt);
-  std::printf("\033[31m[E] ");
-  std::vprintf(fmt, args);
-  std::printf("\033[0m");
-  va_end(args);
-  exit(1);
+	_print_time();
+	va_list args;
+	va_start(args, fmt);
+	std::printf("\033[31m [E] ");
+	std::printf("\033[0m");
+	std::vprintf(fmt, args);
+	va_end(args);
+	exit(1);
 }
 }
--- test/test.cc
+++ test/test.cc
@@ -1,12 +1,10 @@
 #include "test.h"
 
 #include <clock.h>
+#include <log.h>
 
 int main() {
-  moc::clock c;
-  while (true) {
-    auto used = c.tick(1000);
-    printf("last frame computed for %d ms\n", used);
-  }
+  moc::logf("Test from %d\n", 233);
+  moc::panicf("Error from %d\n", 234);
   return 0;
 }
